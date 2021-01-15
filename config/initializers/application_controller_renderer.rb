8d721d791645fbaa563037138565dd1527719d80
okdhrk <okdhrk1014@gmail.com>
2021-01-15 13:19:41 +0000

    image Post ok

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9cbfdafed918e125c390bfd709bdec5ec5467b143ca23ea13de7a063800b990c5d403ed75876e5a255a9598eb8776738dabdd5b86997e13fde7d4f58567946bf'
